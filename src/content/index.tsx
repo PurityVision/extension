480f6ae4074d0803525936d8fde712c74a42199d
Gradey Cullins <gradeycullins@gmail.com>
2023-11-11 10:48:44 -0700

    Always populate license data in popup even if license expired
    - improve error handling when making requests to pv API

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -2,58 +2,63 @@ import browser from 'webextension-polyfill'
 import { filterPage, showFilteredImages } from './filter'
 import { AppStorage, SiteToggleMessage } from '@src/worker'
 import { validateLicense } from '@src/utils'
+import { getLicense } from '@src/api'
 
 let observer: MutationObserver
 
 const onLoad = (): void => {
   const init = async (): Promise<void> => {
-    void tryRunFilter({})
+    try {
+      await tryRunFilter({})
+    } catch (error) {
+      showFilteredImages()
+    }
     browser.runtime.onMessage.addListener(onSiteToggledListener)
   }
 
   init()
-    .then(() => { /* */ })
     .catch(err => console.log(err))
-
-  const mutationCallback = (mutationList: MutationRecord[]): void => {
-    for (const mutation of mutationList) {
-      if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-        const addedImgs: HTMLImageElement[] = []
-        mutation.addedNodes.forEach(node => {
-          if (node.nodeName === 'IMG') {
-            addedImgs.push(node as HTMLImageElement)
+    .finally(() => {
+      const mutationCallback = (mutationList: MutationRecord[]): void => {
+        for (const mutation of mutationList) {
+          if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+            const addedImgs: HTMLImageElement[] = []
+            mutation.addedNodes.forEach(node => {
+              if (node.nodeName === 'IMG') {
+                addedImgs.push(node as HTMLImageElement)
+              }
+            })
+            if (addedImgs.length === 0) {
+              return
+            }
+            void tryRunFilter({ images: addedImgs })
           }
-        })
-        if (addedImgs.length === 0) {
-          return
         }
-        void tryRunFilter({ images: addedImgs })
       }
-    }
-  }
-  observer = new MutationObserver(mutationCallback)
-  observer.observe(window.document, { childList: true, subtree: true })
+      observer = new MutationObserver(mutationCallback)
+      observer.observe(window.document, { childList: true, subtree: true })
+    })
 }
 
 const tryRunFilter = async ({ images }: { images?: HTMLImageElement[] }): Promise<void> => {
   const storage = await browser.storage.local.get() as AppStorage
 
-  try {
-    const licenseValid = validateLicense(storage.license)
-    if (!licenseValid) {
-      console.log('license not valid, not running filter')
-      return
-    }
-    if (!storage.whitelist.includes(window.location.host)) {
-      return
-    }
-    if (images !== undefined) {
-      await filterPage({ license: storage.license, wholePage: false, images })
-    } else {
-      await filterPage({ license: storage.license, wholePage: true })
-    }
-  } catch (err) {
-    console.log(err)
+  const licenseValid = validateLicense(storage.license)
+  if (!licenseValid) {
+    throw new Error('license not valid, not running filter')
+  }
+  const [license, err] = await getLicense(storage.license)
+  if (license === undefined || !license.isValid || license.validityReason !== '') {
+    throw new Error(err?.message)
+  }
+
+  if (!storage.whitelist.includes(window.location.host)) {
+    return
+  }
+  if (images !== undefined) {
+    await filterPage({ license: storage.license, wholePage: false, images })
+  } else {
+    await filterPage({ license: storage.license, wholePage: true })
   }
 }
 
